3de4102ee48498041aa732065828232b554058da
debjyotidutta28 <31996834+debjyotidutta28@users.noreply.github.com>
2024-01-09 12:22:42 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,4 @@
-from fastapi import FastAPI
-import nest_asyncio
-from pyngrok import ngrok
 import uvicorn
 from api import app
 
-ngrok_tunnel = ngrok.connect(8000)
-ngrok_tunnel.public_url = "https://explicitly-moved-mustang.ngrok-free.app"
-print('Public URL:', ngrok_tunnel.public_url)
-nest_asyncio.apply()
-uvicorn.run(app, port=8000)
+uvicorn.run(app, host=0.0.0.0, port=8000)
